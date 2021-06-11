378febdaddd6c4516a2cd8fd33ed538cf01eaf8a
yebh17 <sunnyrebels0@gmail.com>
2021-06-11 18:20:13 +0200

    Add more ssh aliases

--- windows/alias.bat
+++ windows/alias.bat
@@ -6,8 +6,7 @@ doskey clear=cls
 //ssh aliases
 doskey mars=ssh mars@192.168.1.187
 doskey mercury=ssh mercury@192.168.1.186
-doskey pi=ssh pi@192.168.1.74
-doskey mac=ssh sunny@192.168.1.135
+doskey jupiter=ssh jupiter@192.168.1.188
 
 // Git Aliases
 
