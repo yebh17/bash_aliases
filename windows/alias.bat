8eeb10f384bcfe603f6f00a7759991e641a2adac
yebh17 <sunnyrebels0@gmail.com>
2021-06-11 17:11:56 +0200

    Add bat file for aliases in windows

---
+++ windows/alias.bat
@@ -0,0 +1,10 @@
+@echo off
+
+doskey ls=dir
+doskey clear=cls
+
+//ssh aliases
+doskey mars=ssh mars@192.168.1.187
+doskey mercury=ssh mercury@192.168.1.186
+doskey pi=ssh pi@192.168.1.74
+doskey mac=ssh sunny@192.168.1.135
